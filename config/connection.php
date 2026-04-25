ae7cf9a3a17f1420157bcab14167234ee71b7ad6
bhrentlorenzo <bhrentlorenzo05@gmail.com>
2026-04-25 16:52:01 +0800

    Add files via upload

---
+++ config/connection.php
@@ -0,0 +1,22 @@
+<?php
+$host = '127.0.0.1';
+$port = '3307';          // your new port
+$db   = 'dream_job_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    // echo "Connected!";
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
