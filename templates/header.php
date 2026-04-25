c4431bbee8eb61edd936d562db40ca2ac0891c61
bhrentlorenzo <bhrentlorenzo05@gmail.com>
2026-04-25 16:59:02 +0800

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Cloud Engineer Registry</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body class="p-4">
+<div class="container">
+  <h1 class="mb-4">Cloud Engineer Registry</h1>
